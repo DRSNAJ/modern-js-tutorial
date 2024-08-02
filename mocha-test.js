9866985ae8c6d59e4009b44481c07b0262191f42
Randike Jayathilake <randike@corzent.com>
2024-08-02 11:30:45 +0530

    added testing, objects, comparsions

---
+++ mocha-test.js
@@ -0,0 +1,10 @@
+describe("pow", function() {
+    it("2 to the power 2 is 4", function() {
+      assert.equal(pow(2,2),4); 
+    });
+
+    it("4 to the power 4 is 2", function() {
+        assert.equal(pow(4,2),16); 
+      });  
+  
+  });
